a0472b8a8aee0981d6347f820de33007151e5bef
alanbf <alanbf1992@gmail.com>
2024-03-28 22:10:32 -0300

    Primer commit

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+// Iniciar carrusel
+const myCarousel = document.querySelector("#carouselViajes");
+
+const carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 2000,
+  pause: false
+});
+
+carousel.cycle();
+
+// Color del navbar
+
+const scrollOffsetLimit = 400;
+
+$(document).scroll(function() {
+  if ($(this).width() < 768) {
+    return;
+  }
+
+  const offset = window.scrollY;
+
+  transparency = offset/scrollOffsetLimit > 1? 1: offset/scrollOffsetLimit * 1;
+
+  $('#top-navbar').css({
+    "background-color": `rgba(0,139,139,${transparency})`,
+  })
+})
+
+
+// Smooth scrolling
+$(document).on('click', 'a[href^="#"]', function (event) {
+  event.preventDefault();
+
+  $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+  }, 500);
+});
